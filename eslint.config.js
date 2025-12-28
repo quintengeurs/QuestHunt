65d089b4eb182cc6124de819ecac87d953a6408b
quintengeurs <quinten.geurs@gmail.com>
2025-12-28 10:37:59 +0000

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,7 @@
-// eslint.config.js
 import js from "@eslint/js";
-import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import react from "eslint-plugin-react";
 import globals from "globals";
 
 export default [
@@ -21,7 +20,7 @@ export default [
     },
     rules: {
       ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,  // for React 17+ new JSX transform
+      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
